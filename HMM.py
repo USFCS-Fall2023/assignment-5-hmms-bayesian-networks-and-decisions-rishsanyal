35531d180668fee92e0f9c07e84217856a17f9ea
Rishab Sanyal <rsanyal@ucdavis.edu>
2023-11-01 18:18:36 -0700

    Adding cat test files for self practice and testing

--- HMM.py
+++ HMM.py
@@ -6,6 +6,12 @@ import codecs
 import os
 import numpy
 
+from pprint import pprint
+
+import sys
+
+from collections import defaultdict
+
 # observations
 class Observation:
     def __init__(self, stateseq, outputseq):
@@ -30,11 +36,36 @@ class HMM:
         self.transitions = transitions
         self.emissions = emissions
 
-    ## part 1 - you do this.
-    def load(self, basename):
+        self.states = list(self.transitions.keys())
+        self.states.remove("#")
+
+    ## part 1 - you do this. - Done
+    @staticmethod
+    def load(basename):
         """reads HMM structure from transition (basename.trans),
         and emission (basename.emit) files,
         as well as the probabilities."""
+
+        transitions = defaultdict(dict)
+        emissions = defaultdict(dict)
+
+        with open(basename + ".emit", "r") as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    line = line.split()
+                    emissions[line[0]].update({line[1]: float(line[2])})
+
+
+        with open(basename + ".trans", "r") as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    line = line.split()
+                    transitions[line[0]].update({line[1]: float(line[2])})
+
+
+        return HMM(transitions, emissions)
 
 
 
@@ -42,7 +73,53 @@ class HMM:
     def generate(self, n):
         """return an n-length observation by randomly sampling from this HMM."""
 
+        initial_state = self.transitions["#"]
 
+        states = []
+        observations = []
+
+        state = numpy.random.choice(list(initial_state.keys()), p=list(initial_state.values()))
+
+        states.append(state)
+
+        for i in range(n-1):
+            initial_state = self.transitions[state]
+            state = numpy.random.choice(list(initial_state.keys()), p=list(initial_state.values()))
+
+            states.append(state)
+
+        for state in states:
+            initial_state = self.emissions[state]
+            observation = numpy.random.choice(list(initial_state.keys()), p=list(initial_state.values()))
+
+            observations.append(observation)
+
+        return observations, states
+
+
+    def forward(self, observation):
+        ## Implement forward algorithm here.
+
+        num_states = len(self.states)
+        num_obs = len(observation)
+
+        print("The current observation is : ", observation)
+
+        # Initialize the forward matrix
+        forward_matrix = numpy.zeros((num_states, num_obs))
+
+        for i, curr_state in enumerate(self.states):
+            forward_matrix[i][0] = self.transitions['#'][curr_state] * self.emissions[curr_state].get(observation[0], 0)
+
+        for i in range(1, num_obs):
+            for state_index, curr_state in enumerate(self.states):
+                curr_sum = 0
+                for j, prev_state in enumerate(self.states):
+                    curr_sum += forward_matrix[j][i-1] * self.transitions[prev_state][curr_state] * self.emissions[curr_state].get(observation[i], 0)
+                forward_matrix[state_index][i] = curr_sum
+
+
+        return forward_matrix
 
     ## you do this: Implement the Viterbi alborithm. Given an Observation (a list of outputs or emissions)
     ## determine the most likely sequence of states.
@@ -56,4 +133,42 @@ class HMM:
 
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=str, help="provide the name of the file")
+    parser.add_argument("--generate", type=int, help="provide the length of the output sequence", required=False)
+    parser.add_argument("--forward", type=str, help="provide the file name of observation", required=False)
 
+
+    parser.add_argument("--transitions", help="provide the file name of observation", action='store_true')
+    parser.add_argument("--emissions", help="provide the file name of observation", action='store_true')
+
+    args = parser.parse_args()
+
+    model = HMM.load(args.filename)
+
+    if args.transitions:
+        pprint(model.transitions)
+    if args.emissions:
+        pprint(model.emissions)
+
+    if args.transitions or args.emissions:
+        sys.exit(0)
+
+    if args.generate:
+        a, b = model.generate(args.generate)
+
+        print("-"*50)
+        print(a)
+        print("\n")
+        print(b)
+        print("-"*50)
+
+    if args.forward:
+        with open(args.forward, "r") as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    line = line.split()
+                    print(model.forward(line))
+                    break
